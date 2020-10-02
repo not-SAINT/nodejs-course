8d5a858b481600667c8a7a35a323898b7c3ab771
Aleksandr Sharikov <notsaint.mail@gmail.com>
2020-10-02 23:54:32 +0300

    docs: add README.md

--- src/caesar-cipher-cli/checkArguments.js
+++ src/caesar-cipher-cli/checkArguments.js
@@ -5,6 +5,14 @@ const checkArguments = async ({ shift, action }) => {
     errorHandler('argument "shift" is not number', 8);
   }
 
+  if (shift === undefined) {
+    errorHandler('argument "shift" is required', 8);
+  }
+
+  if (action === undefined) {
+    errorHandler('argument "action" is required', 8);
+  }
+
   if (action !== 'encode' && action !== 'decode') {
     errorHandler(
       'argument "action" has not valid value ["encode"|"decode"]',
